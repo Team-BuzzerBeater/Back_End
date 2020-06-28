58cc78868dc9eaaf9e0b50c19c559dcb30eae9a1
artiiicy <mjun09@naver.com>
2020-06-28 21:44:36 +0900

    update: query

--- buzzerbeater/model/printXg.js
+++ buzzerbeater/model/printXg.js
@@ -2,12 +2,12 @@ const pool = require('../module/db/pool');
 
 module.exports = {
     teamList : async() => {
-        var result = await pool.queryParam_None(`SELECT * FROM team`);
+        var result = await pool.queryParam_None(`SELECT teamIdx, teamName FROM team`);
         return result;
     },
 
     player : async(teamIdx) => {
-        var result = await pool.queryParam_None(`SELECT * From player WHERE teamIdx = ${teamIdx}`)
+        var result = await pool.queryParam_None(`SELECT playerIdx, playerName, teamIdx From player WHERE teamIdx = ${teamIdx}`)
         return result;
     }
 };
